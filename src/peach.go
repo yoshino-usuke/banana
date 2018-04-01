de1a99798d20381c909112a62319377a03d5b8da
吉野由祐 <yoshino@yoshino-network.local>
2018-04-01 18:06:31 +0900

    test

--- src/peach.go
+++ src/peach.go
@@ -3,7 +3,6 @@ package main
 import (
 	"./helper"
 	"log"
-	"fmt"
 	"net/http"
 	"os"
 	"encoding/json"
@@ -11,6 +10,8 @@ import (
 	"time"
 	"io/ioutil"
 	"net/url"
+	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/gin-gonic/gin"
 )
 
 // ReceivedMessage ...
@@ -48,14 +49,62 @@ type SendMessage struct {
 	Content   Content  `json:"content"`
 }
 
-func main() {
-	http.HandleFunc("/", helloHandler)
-	http.HandleFunc("/callback", callbackHandler)
-	port := os.Getenv("PORT")
-	addr := fmt.Sprintf(":%s", port)
-	http.ListenAndServe(addr, nil)
-}
+// func main() {
+// 	http.HandleFunc("/", helloHandler)
+// 	http.HandleFunc("/callback", callbackHandler)
+// 	port := os.Getenv("PORT")
+// 	addr := fmt.Sprintf(":%s", port)
+// 	http.ListenAndServe(addr, nil)
+// }
 
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	// ② LINE bot instanceの作成
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	// ③ LINE Messaging API用の Routing設定
+	router.POST("/callback", func(c *gin.Context) {
+		events, err := bot.ParseRequest(c.Request)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				log.Print(err)
+			}
+			return
+		}
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+	})
+
+	router.Run(":" + port)
+}
 func helloHandler(w http.ResponseWriter, r *http.Request)  {
 
 }
