8ecf8bebcae056e1fe404ed4f54fb78beb869f6c
吉野由祐 <yoshino@yoshino-network.local>
2018-05-19 16:23:41 +0900

    https://github.com/k2wanko/appengine-line-bot/tree/master/simple をパクる

--- src/gae/peach.go
+++ src/gae/peach.go
@@ -1,25 +1,28 @@
 package gae
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"net/http"
-	"github.com/dongri/line-bot-sdk-go/linebot"
+	"net/url"
 	"os"
-	"fmt"
+
+	//"google.golang.org/appengine"
+	//"google.golang.org/appengine/log"
+	"google.golang.org/appengine/taskqueue"
+	"google.golang.org/appengine/urlfetch"
+
+	"golang.org/x/net/context"
+
 	"github.com/joho/godotenv"
+	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/line/line-bot-sdk-go/linebot/httphandler"
 	"log"
+	"google.golang.org/appengine"
 )
 
-var botClient *linebot.Client
-
-type Data struct {
-	Secretchannel string
-	Channelkey string
-}
-
-// BotEventHandler ...
-type BotEventHandler struct{
-	linebot.EventHandler
-}
+var bot *linebot.Client
+var botHandler *httphandler.WebhookHandler
 
 func init() {
 
@@ -27,39 +30,92 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-	channelAccessToken := os.Getenv("LINE_CHANNEL_ACCESSTOKEN")
-	channelSecret := os.Getenv("LINE_CHANNEL_SECRET")
-
-	botClient = linebot.NewClient(channelAccessToken)
-	botClient.SetChannelSecret(channelSecret)
-
-	// EventHandler
-	var myEvent linebot.EventHandler = NewEventHandler()
-	botClient.SetEventHandler(myEvent)
-	http.HandleFunc("/callback", callbackHandler)
+	botHandler, err = httphandler.New(
+		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
+		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
+	)
+	if err != nil{
+		log.Fatalln(err)
+	}
+	botHandler.HandleEvents(handleCallback)
 }
 
-func callbackHandler(w http.ResponseWriter, r *http.Request) {
+func main() {
 
-	port := os.Getenv("PORT")
-	addr := fmt.Sprintf(":%s", port)
-	http.ListenAndServe(addr, nil)
-	fmt.Fprint(w, "OK")
-	fmt.Fprintln(w,botClient)
 }
 
-
-// NewEventHandler ...
-func NewEventHandler() *BotEventHandler {
-	return new(BotEventHandler)
+// Webhook を受け取って TaskQueueに詰める関数
+func handleCallback(evs []*linebot.Event, r *http.Request) {
+	c := newContext(r)
+	ts := make([]*taskqueue.Task, len(evs))
+	for i, e := range evs {
+		j, err := json.Marshal(e)
+		if err != nil {
+			errorf(c, "json.Marshal: %v", err)
+			return
+		}
+		data := base64.StdEncoding.EncodeToString(j)
+		t := taskqueue.NewPOSTTask("/task", url.Values{"data": {data}})
+		ts[i] = t
+	}
+	taskqueue.AddMulti(c, ts, "")
 }
 
-func (be *BotEventHandler) OnTextMessage(source linebot.EventSource, replyToken, text string) {
-	message := linebot.NewTextMessage(text + "じゃねぇよ！")
-	result, err := botClient.ReplyMessage(replyToken, message)
-	if err != nil {
-		log.Fatal(err)
+func handleTask(w http.ResponseWriter, r *http.Request) {
+	c := newContext(r)
+	data := r.FormValue("data")
+	if data == "" {
+		errorf(c, "No data")
+		return
 	}
 
-	fmt.Println(result)
+	j, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		errorf(c, "base64 DecodeString: %v", err)
+		return
+	}
+
+	e := new(linebot.Event)
+	err = json.Unmarshal(j, e)
+	if err != nil {
+		errorf(c, "json.Unmarshal: %v", err)
+		return
+	}
+
+	bot, err := newLINEBot(c)
+	if err != nil {
+		errorf(c, "newLINEBot: %v", err)
+		return
+	}
+
+	logf(c, "EventType: %s\nMessage: %#v", e.Type, e.Message)
+
+	m := linebot.NewTextMessage("ok")
+	if _, err = bot.ReplyMessage(e.ReplyToken, m).WithContext(c).Do(); err != nil {
+		errorf(c, "ReplayMessage: %v", err)
+		return
+	}
+
+	w.WriteHeader(200)
+}
+
+func newLINEBot(c context.Context) (*linebot.Client, error) {
+	return botHandler.NewClient(
+		linebot.WithHTTPClient(urlfetch.Client(c)),
+	)
+}
+
+// newContext は appengine.NewContext を短く書くための関数
+func newContext(r *http.Request) context.Context {
+	return appengine.NewContext(r)
+}
+
+// logf は log.Infof を短く書くための関数
+func logf(c context.Context, format string, args ...interface{}) {
+	log.Infof(c, format, args...)
+}
+
+// errorf は log.Errorf を短く書くための関数
+func errorf(c context.Context, format string, args ...interface{}) {
+	log.Errorf(c, format, args...)
 }
--- src/helper/parse.go
+++ src/helper/parse.go
@@ -6,12 +6,17 @@ import (
 	"log"
 )
 
+const TARGET_URL = "http://www.dmm.co.jp"
 type Result struct {
 	rank        string
 	url         string
 	name        string
 	img         string
 	information string
+}
+
+func(r *Result) setUrl(url string){
+	r.url = TARGET_URL + url
 }
 
 func Extract(res http.Response) []Result {
@@ -26,7 +31,7 @@ func Extract(res http.Response) []Result {
 		var result Result
 		result.rank = s.Find("span.rank").Text()
 		url, _ := val.Attr("href")
-		result.url = url
+		result.setUrl(url)
 		result.name = val.Text()
 		img, _ := s.Find("img").Attr("src")
 		result.img = img
